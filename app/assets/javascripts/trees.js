7e81d1f459e347bd344c1652a27c5a66d6785581
Iain Watt <iainjameswatt@gmail.com>
2015-03-13 11:28:45 +0000

    minor styling changes..sorry to do it in dev

--- app/assets/javascripts/trees.js
+++ app/assets/javascripts/trees.js
@@ -1,8 +1,7 @@
 
-
 $(document).ready(function(){  
 
-  $('#species-select').on('change', search);
+$('#species-select').on('change', search);
 
 
 
@@ -12,7 +11,7 @@ function initialize() {
 
     var mapOptions = {
       center: new google.maps.LatLng(37.42291810, -100.08542120),
-      zoom: 4,
+      zoom: 2,
       scrollwheel: false,
       styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}]
     };
@@ -55,9 +54,9 @@ function addAjaxMarker(map) {
 
 
 
-          map: window.map,
-          title: value.species
-        });
+        //   map: window.map,
+        //   title: value.species
+        // });
 
 
         var infoWindowContent = '<div id="info-window-content">' + '<a href="' + window.location.origin + '/trees' + value.id + '"><h5>' + value.name + '</h5></a>' + '<h5>' + value.species + '</h5><p>' + value.description + '</p>' + '</div>';
@@ -190,11 +189,11 @@ function AddNewMarkers(marker) {
 fillDropdown();
 google.maps.event.addDomListener(window, 'load', initialize);
 
-});
 
+});
 google.maps.Map.prototype.clearOverlays = function() {
   for (var i = 0; i < markersArray.length; i++ ) {
     markersArray[i].setMap(null);
-  }
-  markersArray.length = 0;
-}
+    }
+    markersArray.length = 0;
+    }
